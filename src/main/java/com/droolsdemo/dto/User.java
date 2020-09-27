49e25003b3bd3bd1430a67223ab659ace65c7499
xp <310715321@qq.com>
2020-09-27 18:02:26 +0800

    droolsDemo

---
+++ src/main/java/com/droolsdemo/dto/User.java
@@ -0,0 +1,15 @@
+package com.droolsdemo.dto;
+import lombok.Data;
+@Data
+public class User {
+    private String name;
+    private Integer age;
+
+
+    public User(){ }
+
+    public User(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+}
---
+++ src/test/java/com/droolsdemo/DroolsdemoApplicationTests.java
@@ -0,0 +1,30 @@
+package com.droolsdemo;
+
+import com.droolsdemo.dto.User;
+import org.junit.jupiter.api.Test;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroolsdemoApplicationTests {
+
+    private static KieContainer container = null;
+    private KieSession statefulKieSession = null;
+
+    @Test
+    public void test(){
+        KieServices kieServices = KieServices.Factory.get();
+        container = kieServices.getKieClasspathContainer();
+        statefulKieSession = container.newKieSession("myAgeSession");
+        User user = new User("duval yang",12);
+        // 插入一个新的对象
+        statefulKieSession.insert(user);
+        // 触发所有规则
+        statefulKieSession.fireAllRules();
+        //释放所有当前会话资源，为垃圾回收设置会话
+        statefulKieSession.dispose();
+    }
+
+}
